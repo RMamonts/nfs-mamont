e8055a8fee0ba3578eb01cf14253422e281c84b4
Igor Erin <igor.erin.a@gmail.com>
2025-08-10 16:21:23 +0300

    missing file: utils

---
+++ src/utils/error.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+pub fn io_other<T, E>(message: E) -> io::Result<T>
+where
+    E: Into<Box<dyn std::error::Error + Send + Sync>>,
+{
+    Err(io::Error::new(io::ErrorKind::Other, message))
+}
