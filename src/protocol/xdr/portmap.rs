65686d90c2522e0812966ff6f4a4bf1ef7806bfc
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2025-08-27 18:28:02 +0300

    fix: pmaplist Deserialize

--- src/protocol/xdr/portmap.rs
+++ src/protocol/xdr/portmap.rs
@@ -57,15 +57,8 @@ impl Serialize for pmaplist {
 impl Deserialize for pmaplist {
     fn deserialize<R: Read>(src: &mut R) -> std::io::Result<Self> {
         let map = deserialize::<mapping>(src)?;
-        let next = deserialize::<Option<bool>>(src)?;
-
-        if let Some(true) = next {
-            let pmaplist = deserialize::<pmaplist>(src)?;
-
-            Ok(pmaplist { map, next: Box::new(Some(pmaplist)) })
-        } else {
-            Ok(pmaplist { map, next: Box::new(None) })
-        }
+        let next = deserialize::<Option<pmaplist>>(src)?;
+        Ok(pmaplist { map, next: Box::new(next) })
     }
 }
 
