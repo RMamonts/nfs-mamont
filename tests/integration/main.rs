36b0dd63a48e3dc20c6f35aeebf697020010b56c
Igor Erin <igor.erin.a@gmail.com>
2025-10-04 15:37:01 +0300

    refactor: cringe

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -5,6 +5,7 @@ mod random;
 mod utils;
 
 use std::process::Command;
+use std::time::Duration;
 
 const EXPORT_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", "exports");
 
@@ -12,19 +13,16 @@ const EXPORT_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", "exports");
 fn integration_test() {
     let mut random = random::Random::default();
 
-    // let mut demofs = Command::new("cargo")
-    //     .args(&["run", "--example", "demofs"])
-    //     .spawn()
-    //     .unwrap();
+    let mut demofs = Command::new("cargo").args(["run", "--example", "demofs"]).spawn().unwrap();
 
     // wait for mount
-    // std::thread::sleep(Duration::from_secs(10));
+    std::thread::sleep(Duration::from_secs(2));
 
     let mut mount = Command::new("sudo")
-        .args(["mount", "-t", "nfs", "127.0.0.1:/", "/home/ierin/github/nfs-mamont/exports"])
+        .args(["mount", "-t", "nfs", "127.0.0.1:/", EXPORT_DIR])
         .spawn()
         .unwrap();
-    mount.wait().unwrap();
+    assert!(mount.wait().unwrap().success());
 
     eprintln!("starting file tests");
     file::create_write_read_delete(&mut random);
@@ -32,6 +30,6 @@ fn integration_test() {
     eprintln!("starting dir tests");
     dir::create_read_delete();
 
-    // demofs.kill().unwrap();
-    // demofs.wait().unwrap();
+    demofs.kill().unwrap();
+    demofs.wait().unwrap();
 }
