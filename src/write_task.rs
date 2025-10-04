b29c0dc9aa7d3faa18a44af92fa25b46f588c5c3
Mikhail <svmk17@mail.ru>
2025-10-04 13:37:39 +0300

    fix: add error serialization by RMS - move write_fragment out of trait

--- src/write_task.rs
+++ src/write_task.rs
@@ -1,13 +1,15 @@
 use std::io;
 use std::io::{Cursor, Error};
-use tokio::io::AsyncWriteExt;
 use tokio::net::tcp::OwnedWriteHalf;
 use tokio::sync::mpsc::UnboundedReceiver;
 use tracing::{debug, error};
 
+use crate::response_buffer::write_fragment;
 use crate::tcp::CommandResult;
 use crate::xdr::rpc::{accepted_reply, opaque_auth, reply_body, rpc_body, rpc_msg};
 use crate::xdr::{ProtocolErrors, Serialize};
+
+const ERROR_BUFFER_SIZE: usize = 50;
 
 /// An asynchronous task responsible for writing [`VfsTask`] responses to a network connection.
 pub struct WriteTask {
@@ -36,13 +38,16 @@ impl WriteTask {
     async fn run(mut self) -> Result<(), Error> {
         while let Some((xid, result)) = self.result_receiver.recv().await {
             match result {
-                Ok(mut response_buffer) => {
-                    if let Err(e) = response_buffer.write_fragment(&mut self.writehalf).await {
+                Ok(response_buffer) => {
+                    if let Err(e) =
+                        write_fragment(&mut response_buffer.into_inner(), &mut self.writehalf).await
+                    {
                         error!("Write error {:?}", e);
                         return Err(e);
                     }
                 }
                 Err(e) => {
+                    debug!("Error to send: {:?}", e);
                     return match self.error_replying(xid, e).await {
                         Ok(_) => {
                             debug!("Replying successfully to client with error");
@@ -52,7 +57,7 @@ impl WriteTask {
                             error!("Failed to send error reply to client: {:?}", e);
                             Err(e)
                         }
-                    }
+                    };
                 }
             }
         }
@@ -62,22 +67,26 @@ impl WriteTask {
 
     async fn error_replying(&mut self, xid: u32, error: ProtocolErrors) -> io::Result<()> {
         debug!("Replying with protocol error : {:?}", error);
-        let mut buf = Cursor::new(vec![0_u8; 450]);
+        let mut buf = Cursor::new(Vec::with_capacity(ERROR_BUFFER_SIZE));
         match error {
             ProtocolErrors::RpcRejected(e) => {
                 rpc_msg { xid, body: rpc_body::REPLY(reply_body::MSG_DENIED(e)) }
                     .serialize(&mut buf)?;
-                self.writehalf.write_all(&buf.into_inner()).await
+                write_fragment(&mut buf.into_inner(), &mut self.writehalf).await
             }
             ProtocolErrors::RpcAccepted(e) => {
-                reply_body::MSG_ACCEPTED(accepted_reply {
-                    verf: opaque_auth::default(),
-                    reply_data: e,
-                })
+                rpc_msg {
+                    xid,
+                    body: rpc_body::REPLY(reply_body::MSG_ACCEPTED(accepted_reply {
+                        verf: opaque_auth::default(),
+                        reply_data: e,
+                    })),
+                }
                 .serialize(&mut buf)?;
-                self.writehalf.write_all(&buf.into_inner()).await
+                write_fragment(&mut buf.into_inner(), &mut self.writehalf).await
             }
             _ => {
+                // Setup for errors of Mount and Nfs
                 todo!()
             }
         }
