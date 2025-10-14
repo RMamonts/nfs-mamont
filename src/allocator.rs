9a5a541e4df186ce7be26f837ef6dd1f38d0bd2e
Igor Erin <igor.erin.a@gmail.com>
2025-10-14 20:11:03 +0300

    docs

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,28 +9,26 @@ use tokio::sync::mpsc;
 // TODO(i.erin) more tests!!!
 // TODO(i.erin) iterator over buffer chain?
 
-/// Intrusive linked list of buffers.
+/// Instrusive linked list heap allocated node layout.
+///
+/// Node allocated with Global allocator.
 #[repr(C)]
 struct BufferLayout {
     // Pointer to the next entry of the intrusive linked list.
     next: Option<Buffer>,
-
     // Allocation layout.
     layout: Layout,
-
     // Actuall data.
     payload: [u8],
 }
 
+/// Pointer to the heap allocated intrusive linked list node.
 struct Buffer(NonNull<BufferLayout>);
 
 impl Buffer {
-    const HEADER_BUFFER_FIELDS: &[Layout] =
-        &[Layout::new::<Option<Buffer>>(), Layout::new::<Layout>(), Layout::new::<usize>()];
-
-    pub fn alloc(size: usize) -> Buffer {
-        // TODO(i.erin)
-        let layout = Self::compute_layout(size).unwrap();
+    /// Allocates intrusive linked list node ([`BufferLayout`]) and return pointer to it.
+    pub fn alloc(size: usize) -> Self {
+        let layout = Self::compute_layout(size).expect("to construct correct Layout");
 
         // Safety: non zero layout size.
         assert!(layout.size() != 0);
@@ -55,7 +53,9 @@ impl Buffer {
         unsafe { Buffer(NonNull::new_unchecked(ptr)) }
     }
 
-    // Safety: should be called only once for pointer created via [`Self::alloc`].
+    /// Deallocates buffer.
+    ///
+    /// # Safety: should be called only once for pointer created via [`Self::alloc`].
     pub unsafe fn dealloc(&mut self) {
         let ptr = self.0.as_ptr();
 
@@ -63,22 +63,27 @@ impl Buffer {
         unsafe { alloc::dealloc(ptr as *mut _, layout) };
     }
 
+    /// Returns mutable slice to inner buffer.
     pub fn as_mut(&mut self) -> &mut [u8] {
         self.as_mut_and_next().0
     }
 
+    /// Return shared slice to inner buffer.
     pub fn as_slice(&self) -> &[u8] {
         unsafe { &(*self.as_ptr()).payload }
     }
 
+    /// Length of inner buffer.
     pub fn len(&self) -> usize {
         self.as_slice().len()
     }
 
+    /// Mutable reference to next linked list entry.
     pub fn mut_next(&mut self) -> &mut Option<Buffer> {
         self.as_mut_and_next().1
     }
 
+    /// Mutable reference to inner buffer and next linked list entry.
     pub fn as_mut_and_next(&mut self) -> (&mut [u8], &mut Option<Buffer>) {
         // Safety:
         // We use signle linked list --- then user has now way to
@@ -93,6 +98,14 @@ impl Buffer {
         (as_mut, next)
     }
 
+    /// Return raw pointer to heap allocated linked list node.
+    pub fn as_ptr(&self) -> *const BufferLayout {
+        self.0.as_ptr()
+    }
+
+    const HEADER_BUFFER_FIELDS: &[Layout] =
+        &[Layout::new::<Option<Buffer>>(), Layout::new::<Layout>(), Layout::new::<usize>()];
+
     fn compute_layout(payload_size: usize) -> Result<Layout, LayoutError> {
         let mut layout = Layout::from_size_align(0, 1)?;
 
@@ -106,37 +119,31 @@ impl Buffer {
 
         Ok(full_layout.pad_to_align())
     }
-
-    fn layout(&self) -> Layout {
-        unsafe { (*self.0.as_ptr()).layout }
-    }
-
-    fn as_ptr(&self) -> *const BufferLayout {
-        self.0.as_ptr()
-    }
 }
 
 type Sender = mpsc::Sender<Buffer>;
 type Receiver = mpsc::Receiver<Buffer>;
 
-struct BufferChain {
+struct Chain {
     buffer: Option<Buffer>,
     sender: Sender,
 }
 
-impl BufferChain {
+impl Chain {
+    /// Crates new instrusive linked chain of buffers.
     pub fn new(sender: Sender) -> Self {
         Self { buffer: None, sender }
     }
 
-    pub fn push(&mut self, mut buffer: Buffer) {
+    /// Push new intrusive linked list node at head.
+    pub fn push_head(&mut self, mut buffer: Buffer) {
         assert!((*buffer.mut_next()).is_none());
 
         *buffer.mut_next() = self.buffer.take();
         self.buffer = Some(buffer)
     }
 
-    // TODO(i.erin) implement Iterator for that
+    /// Represent linked list chain as vector of mutable slices.
     pub fn to_vec(Self { buffer, .. }: &mut Self) -> Vec<&mut [u8]> {
         let mut result = Vec::new();
 
@@ -154,6 +161,9 @@ impl BufferChain {
         }
     }
 
+    /// Deallocate in blocking manner.
+    ///
+    /// May deadlock in async context.
     pub fn blocking_dealloc(Self { buffer: mut next_buffer, sender }: Self) {
         while let Some(mut buffer) = next_buffer {
             next_buffer = buffer.mut_next().take();
@@ -161,6 +171,7 @@ impl BufferChain {
         }
     }
 
+    /// Deallocate in async manner.
     pub async fn dealloc(Self { buffer: mut next_buffer, sender }: Self) {
         while let Some(mut buffer) = next_buffer {
             next_buffer = buffer.mut_next().take();
@@ -169,6 +180,7 @@ impl BufferChain {
     }
 }
 
+/// Allocates instrusive linked [`Chain`].
 struct Allocator {
     receiver: Receiver,
     sender: Sender,
@@ -176,6 +188,12 @@ struct Allocator {
 }
 
 impl Allocator {
+    /// Crates new allocator with specified buffer size and count.
+    ///
+    /// # Parameters
+    ///
+    /// * `size` --- size of each buffer
+    /// * `count` --- counts of buffer
     pub async fn new(size: usize, count: usize) -> Self {
         let (sender, receiver) = mpsc::channel::<Buffer>(count);
 
@@ -187,15 +205,16 @@ impl Allocator {
         Self { sender, receiver, capacity: size * count }
     }
 
-    pub async fn alloc(&mut self, mut size: usize) -> BufferChain {
+    /// Allocates [`Chain`] at least the specified size.
+    pub async fn alloc(&mut self, mut size: usize) -> Chain {
         assert!(size < self.capacity);
 
-        let mut chain = BufferChain::new(self.sender.clone());
+        let mut chain = Chain::new(self.sender.clone());
         while size > 0 {
             let buffer = self.receiver.recv().await.expect("channel not to be closed");
 
             size = size.saturating_sub(buffer.len());
-            chain.push(buffer);
+            chain.push_head(buffer);
         }
 
         chain
@@ -206,42 +225,44 @@ impl Allocator {
 mod test {
     use super::*;
 
-    // Checks that: we can use public [`Buffer`] api.
+    // Check that we can:
+    // - allocate buffer
+    // - write all of it
+    // - read written values back
+    // - deallocate buffer
     #[test]
-    fn payload_len() {
+    fn alloc_write_dealloc() {
         const SIZE: usize = 12345;
-        const VALUE: &[u8] = &[1, 2, 3, 4];
 
         let mut buffer = Buffer::alloc(SIZE);
-
         assert_eq!(buffer.as_mut().len(), SIZE);
 
-        let mut_slice = buffer.as_mut();
-        for (&value, buffer) in VALUE.iter().zip(mut_slice.iter_mut()) {
+        let value = (0..SIZE).map(|item| item as u8);
+        for (value, buffer) in value.clone().zip(buffer.as_mut().iter_mut()) {
             *buffer = value;
         }
-        assert_eq!(&mut_slice[0..VALUE.len()], VALUE);
+        assert!(buffer.as_mut().iter().enumerate().all(|(index, &value)| index as u8 == value));
 
-        // Safety: same buffer, only called only once
+        // Safety: same buffer, called only once.
         unsafe {
             buffer.dealloc();
         }
     }
 
-    // Checks that: we can use public [`Buffer`] api.
+    // Checks that:
+    // TODO(more test!!!)
     #[test]
     fn buffer_chain() {
         const BUFFER_COUNT: usize = 4;
         const SIZE: usize = 12345;
-        const VALUE: &[u8] = &[1, 2, 3, 4];
 
         let (sender, _reciever) = mpsc::channel(BUFFER_COUNT);
-        let mut buffer_chain = BufferChain::new(sender);
+        let mut buffer_chain = Chain::new(sender);
 
-        buffer_chain.push(Buffer::alloc(SIZE));
-        buffer_chain.push(Buffer::alloc(SIZE));
+        buffer_chain.push_head(Buffer::alloc(SIZE));
+        buffer_chain.push_head(Buffer::alloc(SIZE));
 
-        let vec = BufferChain::to_vec(&mut buffer_chain);
+        let vec = Chain::to_vec(&mut buffer_chain);
         assert_eq!(vec.len(), 2);
     }
 }
