2b051c78e9dd63c6779a541521b4389b2b122337
artemiipatov <patov.988@gmail.com>
2025-10-28 12:37:23 +0300

    fix: set NFS3_FHSIZE to 8 bytes

--- src/nfsv3.rs
+++ src/nfsv3.rs
@@ -5,7 +5,7 @@ const NFS_PROGRAM: u32 = 100003;
 #[allow(dead_code)]
 const NFS_VERSION: u32 = 3;
 #[allow(dead_code)]
-const NFS3_FHSIZE: u32 = 64;
+const NFS3_FHSIZE: u32 = 8;
 #[allow(dead_code)]
 const NFS3_COOKIEVERFSIZE: u32 = 8;
 #[allow(dead_code)]
