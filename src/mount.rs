f9f3eb4378d4602eafcd087bddda21f52de79e83
Artemii Patov <59678750+artemiipatov@users.noreply.github.com>
2025-11-17 10:40:28 +0300

    feat: file handle const size (#19)
    * feat: file handle const size
    * fix: set MAX_FILE_HANDLE_SIZE to 8 bytes
    * fix: set NFS3_FHSIZE to 8 bytes
    * refactor: remove magic values

--- src/mount.rs
+++ src/mount.rs
@@ -9,9 +9,9 @@ const MNTPATHLEN: u32 = 1024;
 #[allow(dead_code)]
 const MNTNAMLEN: u32 = 255;
 #[allow(dead_code)]
-const FHSIZE3: u32 = 64;
+const FHSIZE3: u32 = 8;
 
-type fhandle3 = Vec<u8>;
+type fhandle3 = [u8; FHSIZE3 as usize];
 type dirpath = Vec<u8>;
 type name = Vec<u8>;
 
