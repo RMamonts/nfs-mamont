7fd0dfab0478a080e6ec11007553769a5612e83d
artemiipatov <patov.988@gmail.com>
2025-12-28 14:41:37 +0300

    feat: add some changes to the initial version of mount description

--- src/mount/mnt.rs
+++ src/mount/mnt.rs
@@ -3,12 +3,11 @@
 //! as defined in RFC 1813 section 5.2.1.
 //! <https://datatracker.ietf.org/doc/html/rfc1813#section-5.2.1>.
 
-#![allow(dead_code)]
-
 use async_trait::async_trait;
 
-use super::{DirPath, Error, FileHandle};
+use super::{DirPath, Error, Handle};
 
+/// Authentication flavors.
 #[derive(Debug)]
 pub enum AuthFlavor {
     None,
@@ -18,13 +17,19 @@ pub enum AuthFlavor {
     Kerb,
 }
 
+/// Success result.
 pub struct Success {
-    pub file_handle: FileHandle,
+    /// The file handle for the mounted directory.
+    /// This file handle may be used in the NFS protocol.
+    pub file_handle: Handle,
+    /// Vector of RPC authentication flavors that are supported with
+    /// the client's use of the file handle (or any file handles derived from it)
     pub auth_flavors: Vec<AuthFlavor>,
 }
 
 pub type Result = std::result::Result<Success, Error>;
 
+/// Defines callback to pass [`Mnt::mnt`] result into.
 #[async_trait]
 pub trait Promise {
     async fn keep(result: Result);
@@ -32,5 +37,12 @@ pub trait Promise {
 
 #[async_trait]
 pub trait Mnt {
+    /// Maps a pathname on the server to a NFS version 3 protocol file handle.
+    ///
+    /// # Parameters:
+    /// * `dirpath` --- a server pathname of a directory.
+    /// 
+    /// This procedure also results in the server adding a new entry 
+    /// to its mount list recording that this client has mounted the directory.
     async fn mnt(&self, dirpath: DirPath, promise: impl Promise);
 }
--- src/mount/mod.rs
+++ src/mount/mod.rs
@@ -5,65 +5,68 @@
 
 pub mod mnt;
 
-pub const MNTPATHLEN: usize = 1024;
-pub const MNTNAMLEN: usize = 255;
-pub const FHSIZE3: usize = 64;
+/// Maximum bytes in a path name.
+pub const MOUNT_DIRPATH_LEN: usize = 1024;
+/// Maximum bytes in a name.
+pub const MOUNT_HOST_NAME_LEN: usize = 255;
+/// Maximum bytes in a NFS v3 file handle.
+pub const HANDLE_SIZE: usize = 8;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Unique file identifier.
+///
+/// Corresponds to the file handle from RFC 1813.
 #[derive(Clone)]
-pub struct FileHandle(pub Vec<u8>);
+pub struct Handle(pub [u8; HANDLE_SIZE]);
 
-impl FileHandle {
-    pub fn new(bytes: Vec<u8>) -> Self {
-        assert!(bytes.len() <= FHSIZE3);
-        Self(bytes)
-    }
-}
-
-/// Directory path
+/// Server pathname of a directory.
 pub type DirPath = String;
 
-/// Client/host name
-pub type Name = String;
+/// Client host name.
+pub type HostName = String;
 
 #[derive(Debug)]
 pub enum Error {
     /// Not owner
-    Permission,
+    Permission = 1,
     /// No such file or directory
-    NoEntry,
+    NoEntry = 2,
     /// I/O error
-    IO,
+    IO = 3,
     /// Permission denied
-    Access,
+    Access = 4,
     /// Not a directory
-    NotDir,
+    NotDir = 5,
     /// Invalid argument
-    InvalidArgument,
+    InvalidArgument = 6,
     /// Filename too long
-    NameTooLong,
+    NameTooLong = 7,
     /// Operation is not supported
-    NotSupported,
+    NotSupported = 8,
     /// A failure on the server
-    ServerFault,
+    ServerFault = 9,
 }
 
+/// Entry of the list maintained on the server of clients 
+/// that have requested file handles with the MNT procedure.
 #[derive(Clone)]
 pub struct MountEntry {
-    pub hostname: Name,
+    /// Name of the client host that is sending RPC.
+    pub hostname: HostName,
+    /// Server pathname of a directory.
     pub directory: DirPath,
 }
 
-#[derive(Clone)]
-pub struct Group {
-    pub name: Name,
-}
-
+/// Export entry, containing list of clients, allowed to
+/// mount the specified directory.
 #[derive(Clone)]
 pub struct ExportEntry {
+    /// Exported directory.
     pub directory: DirPath,
-    pub groups: Vec<Group>,
+    /// Client host names. They are implementation specific
+    /// and cannot be directly interpreted by clients.
+    pub name: Vec<HostName>,
 }
 
 pub trait Mount {}
