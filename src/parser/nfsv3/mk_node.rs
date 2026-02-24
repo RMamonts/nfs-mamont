a68581451dbc18cd7eab592abe6fb2d536c0c885
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-02-25 00:06:49 +0300

    fix: Remove unsupported file types from MkNode enum

--- src/parser/nfsv3/mk_node.rs
+++ src/parser/nfsv3/mk_node.rs
@@ -11,11 +11,11 @@ use std::io::Read;
 
 fn what(src: &mut impl Read) -> Result<mk_node::What> {
     match u32(src)? {
-        1 => Ok(What::Regular),
-        2 => Ok(What::Directory),
+        1 => Err(Error::EnumDiscMismatch),
+        2 => Err(Error::EnumDiscMismatch),
         3 => Ok(What::Block(new_attr(src)?, Device { major: u32(src)?, minor: u32(src)? })),
         4 => Ok(What::Char(new_attr(src)?, Device { major: u32(src)?, minor: u32(src)? })),
-        5 => Ok(What::SymbolicLink),
+        5 => Err(Error::EnumDiscMismatch),
         6 => Ok(What::Socket(new_attr(src)?)),
         7 => Ok(What::Fifo(new_attr(src)?)),
         _ => Err(Error::EnumDiscMismatch),
@@ -35,21 +35,144 @@ pub fn args(src: &mut impl Read) -> Result<mk_node::Args> {
 mod tests {
     use std::io::Cursor;
 
+    use crate::parser::Error;
+    use crate::vfs::mk_node;
+    use crate::vfs::set_attr;
+
+    #[rustfmt::skip]
+    const EMPTY_NEW_ATTR: &[u8] = &[
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+    ];
+
+    #[rustfmt::skip]
+    const ARGS_PREFIX_NODE: &[u8] = &[
+        0x00, 0x00, 0x00, 0x08,
+        0x01, 0x02, 0x03, 0x04,
+        0x05, 0x06, 0x07, 0x08,
+        0x00, 0x00, 0x00, 0x04,
+        b'n', b'o', b'd', b'e',
+    ];
+
+    fn wrap_what_with_args(what_bytes: &[u8]) -> Vec<u8> {
+        [ARGS_PREFIX_NODE, what_bytes].concat()
+    }
+
     #[test]
     fn test_mknod_regular() {
-        #[rustfmt::skip]
-        const DATA: &[u8] = &[
-            0x00, 0x00, 0x00, 0x08, 0x01, 0x02, 0x03, 0x04,
-            0x05, 0x06, 0x07, 0x08, 0x00, 0x00, 0x00, 0x04,
-            b'n', b'o', b'd', b'e', 0x00, 0x00, 0x00, 0x01,
-        ];
-
-        let result = super::args(&mut Cursor::new(DATA)).unwrap();
+        let data = wrap_what_with_args(&[[0, 0, 0, 7].as_slice(), EMPTY_NEW_ATTR].concat());
+        let result = super::args(&mut Cursor::new(data)).unwrap();
 
         assert_eq!(result.dir.0, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
         assert_eq!(result.name, "node");
+        assert!(matches!(
+            result.what,
+            mk_node::What::Fifo(set_attr::NewAttr {
+                mode: None,
+                uid: None,
+                gid: None,
+                size: None,
+                atime: set_attr::SetTime::DontChange,
+                mtime: set_attr::SetTime::DontChange,
+            })
+        ));
+    }
 
-        // TODO()
-        // assert!(matches!(result.what, mk_node::What::Block(todo!(), todo!())));
+    #[test]
+    fn test_what_block() {
+        let data = [
+            [0, 0, 0, 3].as_slice(),
+            EMPTY_NEW_ATTR,
+            [0, 0, 0, 17].as_slice(),
+            [0, 0, 0, 34].as_slice(),
+        ]
+        .concat();
+        let result = super::what(&mut Cursor::new(data)).unwrap();
+
+        assert!(matches!(
+            result,
+            mk_node::What::Block(
+                set_attr::NewAttr {
+                    mode: None,
+                    uid: None,
+                    gid: None,
+                    size: None,
+                    atime: set_attr::SetTime::DontChange,
+                    mtime: set_attr::SetTime::DontChange,
+                },
+                crate::vfs::file::Device { major: 17, minor: 34 }
+            )
+        ));
+    }
+
+    #[test]
+    fn test_what_char() {
+        let data = [
+            [0, 0, 0, 4].as_slice(),
+            EMPTY_NEW_ATTR,
+            [0, 0, 0, 170].as_slice(),
+            [0, 0, 0, 187].as_slice(),
+        ]
+        .concat();
+        let result = super::what(&mut Cursor::new(data)).unwrap();
+
+        assert!(matches!(
+            result,
+            mk_node::What::Char(
+                set_attr::NewAttr {
+                    mode: None,
+                    uid: None,
+                    gid: None,
+                    size: None,
+                    atime: set_attr::SetTime::DontChange,
+                    mtime: set_attr::SetTime::DontChange,
+                },
+                crate::vfs::file::Device { major: 170, minor: 187 }
+            )
+        ));
+    }
+
+    #[test]
+    fn test_what_socket() {
+        let data = [[0, 0, 0, 6].as_slice(), EMPTY_NEW_ATTR].concat();
+        let result = super::what(&mut Cursor::new(data)).unwrap();
+
+        assert!(matches!(
+            result,
+            mk_node::What::Socket(set_attr::NewAttr {
+                mode: None,
+                uid: None,
+                gid: None,
+                size: None,
+                atime: set_attr::SetTime::DontChange,
+                mtime: set_attr::SetTime::DontChange,
+            })
+        ));
+    }
+
+    #[test]
+    fn test_what_invalid_discriminants() {
+        for disc in [1_u32, 2, 5, 8] {
+            let bytes = disc.to_be_bytes();
+            assert!(matches!(super::what(&mut Cursor::new(bytes)), Err(Error::EnumDiscMismatch)));
+        }
+    }
+
+    #[test]
+    fn test_args_invalid_what_discriminant() {
+        let data = wrap_what_with_args(&[0, 0, 0, 1]);
+
+        assert!(matches!(super::args(&mut Cursor::new(data)), Err(Error::EnumDiscMismatch)));
+    }
+
+    #[test]
+    fn test_args_truncated_after_what() {
+        let data = wrap_what_with_args(&[0, 0, 0, 7]);
+
+        assert!(matches!(super::args(&mut Cursor::new(data)), Err(Error::IO(_))));
     }
 }
