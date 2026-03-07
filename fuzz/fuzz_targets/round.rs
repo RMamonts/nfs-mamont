feba3f136e4152ee3fcce476450ae0ea8ca90b3b
MikhailSvirukov <svmk17@mail.ru>
2026-03-07 15:10:30 +0300

    feat: add match for round-trips

--- fuzz/fuzz_targets/round.rs
+++ fuzz/fuzz_targets/round.rs
@@ -1,146 +1,205 @@
 #![no_main]
 
+use std::io::Cursor;
+
 use libfuzzer_sys::fuzz_target;
 use nfs_mamont::client::arguments;
 use nfs_mamont::parser::{mount, nfsv3, Arguments};
-use std::io::Cursor;
+
+const MAX_SLICE_SIZE_ARBITRARY: usize = 1000;
+
+const DEFAULT_CAPACITY: usize =
+    nfs_mamont::parser::parser_struct::DEFAULT_SIZE + MAX_SLICE_SIZE_ARBITRARY;
+
+macro_rules! roundtrip {
+    ($arg:expr, $write:path, $read:path, $variant:path) => {{
+        let mut buf = Cursor::new(vec![0u8; DEFAULT_CAPACITY]);
+        $write(&mut buf, $arg.clone()).unwrap();
+        let len = buf.position();
+        buf.set_position(0);
+        assert_eq!($read(&mut buf).unwrap(), $arg);
+        assert_eq!(buf.position(), len);
+    }};
+}
 
 fuzz_target!(|data: Arguments| {
-    let mut buf = Cursor::new(vec![0u8; 6500]);
     match data {
-        Arguments::GetAttr(arg) => {
-            arguments::nfsv3::get_attr::get_attr_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::get_attr::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::SetAttr(arg) => {
-            arguments::nfsv3::set_attr::set_attr_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::set_attr::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::LookUp(arg) => {
-            arguments::nfsv3::lookup::lookup_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::lookup::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Access(arg) => {
-            arguments::nfsv3::access::access_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::access::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::ReadLink(arg) => {
-            arguments::nfsv3::read_link::read_link_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::read_link::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Read(arg) => {
-            arguments::nfsv3::read::read_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::read::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Write(_) => {}
-        Arguments::Create(arg) => {
-            arguments::nfsv3::create::create_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::create::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::MkDir(arg) => {
-            arguments::nfsv3::mk_dir::mk_dir_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::mk_dir::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::SymLink(arg) => {
-            arguments::nfsv3::symlink::symlink_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::symlink::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::MkNod(arg) => {
-            arguments::nfsv3::mk_node::mk_node_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::mk_node::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Remove(arg) => {
-            arguments::nfsv3::remove::remove_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::remove::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::RmDir(arg) => {
-            arguments::nfsv3::rm_dir::rm_dir_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::rm_dir::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Rename(arg) => {
-            arguments::nfsv3::rename::rename_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::rename::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Link(arg) => {
-            arguments::nfsv3::link::link_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::link::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::ReadDir(arg) => {
-            arguments::nfsv3::read_dir::read_dir_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::read_dir::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::ReadDirPlus(arg) => {
-            arguments::nfsv3::read_dir_plus::read_dir_plus_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::read_dir_plus::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::FsStat(arg) => {
-            arguments::nfsv3::fs_stat::fs_stat_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::fs_stat::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::FsInfo(arg) => {
-            arguments::nfsv3::fs_info::fs_info_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::fs_info::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::PathConf(arg) => {
-            arguments::nfsv3::path_conf::path_conf_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::path_conf::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Commit(arg) => {
-            arguments::nfsv3::commit::commit_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = nfsv3::commit::args(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Mount(arg) => {
-            arguments::mount::mnt::mount_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = mount::mnt::mount(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        Arguments::Unmount(arg) => {
-            arguments::mount::unmnt::unmount_args(&mut buf, arg.clone()).unwrap();
-            buf.set_position(0);
-            let res = mount::umnt::unmount(&mut buf).unwrap();
-            assert_eq!(res, arg)
-        }
-        _ => {}
-    }
+        Arguments::GetAttr(arg) => roundtrip!(
+            arg,
+            arguments::nfsv3::get_attr::get_attr_args,
+            nfsv3::get_attr::args,
+            Arguments::GetAttr
+        ),
+
+        Arguments::SetAttr(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::set_attr::set_attr_args,
+            nfsv3::set_attr::args,
+            Arguments::SetAttr
+        ),
+
+        Arguments::LookUp(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::lookup::lookup_args,
+            nfsv3::lookup::args,
+            Arguments::LookUp
+        ),
+
+        Arguments::Access(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::access::access_args,
+            nfsv3::access::args,
+            Arguments::Access
+        ),
+
+        Arguments::ReadLink(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::read_link::read_link_args,
+            nfsv3::read_link::args,
+            Arguments::ReadLink
+        ),
+
+        Arguments::Read(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::read::read_args,
+            nfsv3::read::args,
+            Arguments::Read
+        ),
+
+        Arguments::Create(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::create::create_args,
+            nfsv3::create::args,
+            Arguments::Create
+        ),
+
+        Arguments::MkDir(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::mk_dir::mk_dir_args,
+            nfsv3::mk_dir::args,
+            Arguments::MkDir
+        ),
+
+        Arguments::SymLink(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::symlink::symlink_args,
+            nfsv3::symlink::args,
+            Arguments::SymLink
+        ),
+
+        Arguments::MkNod(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::mk_node::mk_node_args,
+            nfsv3::mk_node::args,
+            Arguments::MkNod
+        ),
+
+        Arguments::Remove(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::remove::remove_args,
+            nfsv3::remove::args,
+            Arguments::Remove
+        ),
+
+        Arguments::RmDir(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::rm_dir::rm_dir_args,
+            nfsv3::rm_dir::args,
+            Arguments::RmDir
+        ),
+
+        Arguments::Rename(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::rename::rename_args,
+            nfsv3::rename::args,
+            Arguments::Rename
+        ),
+
+        Arguments::Link(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::link::link_args,
+            nfsv3::link::args,
+            Arguments::Link
+        ),
+
+        Arguments::ReadDir(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::read_dir::read_dir_args,
+            nfsv3::read_dir::args,
+            Arguments::ReadDir
+        ),
+
+        Arguments::ReadDirPlus(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::read_dir_plus::read_dir_plus_args,
+            nfsv3::read_dir_plus::args,
+            Arguments::ReadDirPlus
+        ),
+
+        Arguments::FsStat(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::fs_stat::fs_stat_args,
+            nfsv3::fs_stat::args,
+            Arguments::FsStat
+        ),
+
+        Arguments::FsInfo(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::fs_info::fs_info_args,
+            nfsv3::fs_info::args,
+            Arguments::FsInfo
+        ),
+
+        Arguments::PathConf(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::path_conf::path_conf_args,
+            nfsv3::path_conf::args,
+            Arguments::PathConf
+        ),
+
+        Arguments::Commit(arg) => roundtrip!(
+
+            arg,
+            arguments::nfsv3::commit::commit_args,
+            nfsv3::commit::args,
+            Arguments::Commit
+        ),
+
+        Arguments::Mount(arg) => roundtrip!(
+
+            arg,
+            arguments::mount::mnt::mount_args,
+            mount::mnt::mount,
+            Arguments::Mount
+        ),
+
+        Arguments::Unmount(arg) => roundtrip!(
+
+            arg,
+            arguments::mount::unmnt::unmount_args,
+            mount::umnt::unmount,
+            Arguments::Unmount
+        ),
+
+        Arguments::Write(_) => return,
+        _ => {return};
+    };
 });
