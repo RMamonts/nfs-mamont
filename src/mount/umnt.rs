d69f5989720c7b328358a5b272e9d745e6b08453
VYAR <89383982+VadimYarovoy@users.noreply.github.com>
2026-03-09 13:26:53 +0300

    refactor: remove mount interface (#93)

--- src/mount/umnt.rs
+++ src/mount/umnt.rs
@@ -1,32 +1,11 @@
-//! Defines Mount version 3 [`Umnt`] interface (Procedure 3).
+//! Defines Mount version 3 Umnt interface (Procedure 3).
 //!
 //! as defined in RFC 1813 section 5.2.3.
 //! <https://datatracker.ietf.org/doc/html/rfc1813#section-5.2.3>.
 
-use async_trait::async_trait;
-
 use crate::vfs::file;
-
-/// Defines callback to pass [`Umnt::umnt`] result into.
-#[async_trait]
-pub trait Promise {
-    async fn keep();
-}
 
 /// Arguments for the Unmount operation, containing the path to be unmounted.
 #[cfg_attr(test, derive(Eq, PartialEq))]
 #[derive(Debug)]
 pub struct UnmountArgs(pub file::Path);
-
-#[async_trait]
-pub trait Umnt {
-    /// Removes the mount list entry for the directory that was
-    /// previously the subject of a MNT call from this client.
-    ///
-    /// # Parameters:
-    /// * `dirpath` --- a server pathname of a directory.
-    ///
-    /// AUTH_UNIX authentication or better is required.
-    /// There are no MOUNT protocol errors which can be returned from this procedure.
-    async fn umnt(&self, dirpath: file::Path);
-}
