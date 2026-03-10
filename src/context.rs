6b5094bb5d067a71840387c2c007764fdf1b00b3
Vadim Yarovoy <v.yarovoy@yadro.com>
2026-03-10 11:48:45 +0300

    refactor(context): remove ConnectionContext

--- src/context.rs
+++ src/context.rs
@@ -8,7 +8,6 @@ use tokio::net::unix::SocketAddr;
 use tokio::sync::RwLock;
 
 use crate::mount::HostName;
-use crate::rpc::AuthFlavor;
 use crate::vfs::{self, file};
 
 pub type SharedVfs = Arc<dyn vfs::Vfs + Send + Sync + 'static>;
@@ -22,23 +21,17 @@ pub struct ServerExport {
     allowed_hosts: Vec<HostName>,
 }
 
-pub struct ServerContext {
-    settings: ServerSettings,
-    backend: SharedVfs,
-    exports: Arc<RwLock<ExportRegistry>>,
-    mounts: Arc<RwLock<MountRegistry>>,
-}
-
-pub struct ConnectionContext {
-    local_addr: Option<SocketAddr>,
-    client_addr: Option<SocketAddr>,
-    auth: Option<AuthFlavor>,
-}
-
 struct ExportRegistry {
     by_directory: HashMap<file::Path, ServerExport>,
 }
 
 struct MountRegistry {
     by_client: HashMap<SocketAddr, HashSet<file::Path>>,
+}
+
+pub struct ServerContext {
+    settings: ServerSettings,
+    backend: SharedVfs,
+    exports: Arc<RwLock<ExportRegistry>>,
+    mounts: Arc<RwLock<MountRegistry>>,
 }
