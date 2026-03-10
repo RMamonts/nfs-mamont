b512daec6bf779c45a7cf9f79226b104dedb1996
Vadim Yarovoy <v.yarovoy@yadro.com>
2026-03-10 14:54:25 +0300

    refactor(context): make ServerContext generic over Allocator
    - Add Allocator generic type parameter to ServerContext
    - Simplify ExportRegistry to allow only one export per directory

--- src/context.rs
+++ src/context.rs
@@ -7,6 +7,7 @@ use std::sync::Arc;
 use tokio::net::unix::SocketAddr;
 use tokio::sync::RwLock;
 
+use crate::allocator::Allocator;
 use crate::mount::{ExportEntry, MountEntry};
 use crate::vfs::{self, file};
 
@@ -18,16 +19,20 @@ pub struct ServerSettings {
 }
 
 struct ExportRegistry {
-    by_directory: HashMap<file::Path, Vec<ExportEntry>>,
+    // one dir can have only one export
+    by_directory: HashMap<file::Path, ExportEntry>,
 }
 
 struct MountRegistry {
+    // one client can mount multiple dirs
     by_client: HashMap<SocketAddr, HashSet<MountEntry>>,
 }
 
-pub struct ServerContext {
-    settings: ServerSettings,
+pub struct ServerContext<T: Allocator> {
+    allocator: T,
     backend: SharedVfs,
+    // what's available to mount
     exports: Arc<RwLock<ExportRegistry>>,
+    // who has mounted what
     mounts: Arc<RwLock<MountRegistry>>,
 }
