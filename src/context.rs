ed3de2ea7a62a098c778c3107ae431587e68d807
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-03-11 17:35:45 +0300

    refactor(context): remove unused structs and clean up imports in ServerContext

--- src/context.rs
+++ src/context.rs
@@ -1,26 +1,12 @@
 #![allow(dead_code)]
 
-use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
-use tokio::net::unix::SocketAddr;
 use tokio::sync::Mutex;
 
-use crate::allocator::Impl;
-use crate::mount::{ExportEntry, MountEntry};
-use crate::vfs::{self, file};
+use crate::{allocator::Impl, vfs};
 
 pub type SharedVfs = Arc<dyn vfs::Vfs + Send + Sync + 'static>;
-
-struct ExportRegistry {
-    // one dir can have only one export
-    by_directory: HashMap<file::Path, ExportEntry>,
-}
-
-struct MountRegistry {
-    // one client can mount multiple dirs
-    by_client: HashMap<SocketAddr, HashSet<MountEntry>>,
-}
 
 pub struct ServerContext {
     allocator: Arc<Mutex<Impl>>,
