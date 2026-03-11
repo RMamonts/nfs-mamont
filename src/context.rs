71c0bb5ff0dc4f5cc60024ff4b94ed88efe8d564
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-03-11 17:36:29 +0300

    refactor(context): replace SharedVfs type alias with direct Arc usage in ServerContext

--- src/context.rs
+++ src/context.rs
@@ -6,9 +6,7 @@ use tokio::sync::Mutex;
 
 use crate::{allocator::Impl, vfs};
 
-pub type SharedVfs = Arc<dyn vfs::Vfs + Send + Sync + 'static>;
-
 pub struct ServerContext {
     allocator: Arc<Mutex<Impl>>,
-    backend: SharedVfs,
+    backend: Arc<dyn vfs::Vfs + Send + Sync + 'static>,
 }
