6e8246d1cf6fb4accffd8bfde99805729ac61f08
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-03-15 10:46:30 +0300

    refactor(mount): update argument types to use mnt and umnt modules and fx doc

--- src/client/arguments/mount/mnt.rs
+++ src/client/arguments/mount/mnt.rs
@@ -1,10 +1,10 @@
 use std::io;
 use std::io::Write;
 
-use crate::mount::mnt::Args;
+use crate::mount::mnt;
 use crate::serializer::files::file_path;
 
-/// Serializes the arguments [`MountArgs`] for a Mount `MOUNT` operation to the provided `Write` destination.
-pub fn mount_args(dest: &mut impl Write, arg: Args) -> io::Result<()> {
+/// Serializes the arguments [`mnt::Args`] for a Mount `MOUNT` operation to the provided `Write` destination.
+pub fn mount_args(dest: &mut impl Write, arg: mnt::Args) -> io::Result<()> {
     file_path(dest, arg.dirpath)
 }
--- src/client/arguments/mount/unmnt.rs
+++ src/client/arguments/mount/unmnt.rs
@@ -1,10 +1,10 @@
 use std::io;
 use std::io::Write;
 
-use crate::mount::umnt::Args;
+use crate::mount::umnt;
 use crate::serializer::files::file_path;
 
-/// Serializes the arguments [`UnmountArgs`] for a Mount `UNMOUNT` operation to the provided `Write` destination.
-pub fn unmount_args(dest: &mut impl Write, arg: Args) -> io::Result<()> {
+/// Serializes the arguments [`umnt::Args`] for a Mount `UNMOUNT` operation to the provided `Write` destination.
+pub fn unmount_args(dest: &mut impl Write, arg: umnt::Args) -> io::Result<()> {
     file_path(dest, arg.dirpath)
 }
