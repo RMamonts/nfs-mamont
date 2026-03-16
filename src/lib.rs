8a25efb60a6ca41c5eabb4d254db52bbe0dcfd59
VYAR <89383982+VadimYarovoy@users.noreply.github.com>
2026-03-16 16:48:44 +0300

    refactor(connection): encapsulate connection task creation (#169)
    * refactor(connection): encapsulate connection task creation
    
    Resolves #167
    
    * fix: stay with pub for now
    
    * refactor: remove process_socket fn
    
    * fmt
    
    ---------
    
    Co-authored-by: Vadim Yarovoy <v.yarovoy@yadro.com>

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,9 @@ pub mod task;
 pub mod vfs;
 
 use tokio::net::TcpListener;
-use tokio::net::TcpStream;
-use tokio::sync::mpsc;
 
-use crate::task::connection::read::ReadTask;
-use crate::task::connection::vfs::VfsTask;
-use crate::task::connection::write::WriteTask;
-use crate::task::global::mount::{MountCommand, MountTask};
-use crate::task::ProcReply;
+use crate::task::connection;
+use crate::task::global::mount::MountTask;
 
 /// Starts the NFS server and processes client connections.
 pub async fn handle_forever(listener: TcpListener) -> std::io::Result<()> {
@@ -32,20 +27,6 @@ pub async fn handle_forever(listener: TcpListener) -> std::io::Result<()> {
 
         socket.set_nodelay(true)?;
 
-        process_socket(socket, mount_sender.clone()).await;
+        connection::new(socket, mount_sender.clone()).await;
     }
-}
-
-async fn process_socket(socket: TcpStream, mount_sender: mpsc::UnboundedSender<MountCommand>) {
-    let (readhalf, writehalf) = socket.into_split();
-    // channel for result
-    let (result_sender, result_receiver) = mpsc::unbounded_channel::<ProcReply>();
-    // channel for request
-    let (command_sender, command_receiver) = mpsc::unbounded_channel::<()>();
-
-    ReadTask::new(readhalf, command_sender, mount_sender, result_sender.clone()).spawn();
-
-    VfsTask::new(command_receiver, result_sender).spawn();
-
-    WriteTask::new(writehalf, result_receiver).spawn();
 }
