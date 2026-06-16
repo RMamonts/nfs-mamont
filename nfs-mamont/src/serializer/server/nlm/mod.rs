97560086041af3e888c173ce8d8ada6f27607061
MikhailSvirukov <svmk17@mail.ru>
2026-06-16 19:08:10 +0300

    fix: test fix types

--- nfs-mamont/src/serializer/server/nlm/mod.rs
+++ nfs-mamont/src/serializer/server/nlm/mod.rs
@@ -170,7 +170,7 @@ mod tests {
                 holder: Some(Nlm4Holder::new(
                     true,
                     1,
-                    OpaqueHandle::new([0; OPAQUE_HANDLE_SIZE]),
+                    OpaqueHandle::new([0; OPAQUE_HANDLE_SIZE].to_vec()).unwrap(),
                     0,
                     0,
                 )),
@@ -189,11 +189,11 @@ mod tests {
     #[test]
     fn test_res_denied_serializes_full_holder() {
         let holder = Nlm4Holder::new(
-            true,                                          // exclusive
-            12345,                                         // system_identifier
-            OpaqueHandle::new([0xAB; OPAQUE_HANDLE_SIZE]), // opaque_handle
-            99,                                            // lock_offset
-            200,                                           // lock_length
+            true,                                                            // exclusive
+            12345,                                                           // system_identifier
+            OpaqueHandle::new([0xAB; OPAQUE_HANDLE_SIZE].to_vec()).unwrap(), // opaque_handle
+            99,                                                              // lock_offset
+            200,                                                             // lock_length
         );
         let res = Nlm4TestRes {
             cookie: cookie(0xDEADBEEF),
@@ -223,8 +223,13 @@ mod tests {
 
     #[test]
     fn test_res_denied_holder_has_correct_offset_in_buffer() {
-        let holder =
-            Nlm4Holder::new(false, 999, OpaqueHandle::new([0x01; OPAQUE_HANDLE_SIZE]), 0, 0);
+        let holder = Nlm4Holder::new(
+            false,
+            999,
+            OpaqueHandle::new([0x01; OPAQUE_HANDLE_SIZE].to_vec()).unwrap(),
+            0,
+            0,
+        );
         let res = Nlm4TestRes {
             cookie: cookie(0),
             test_stat: Nlm4TestReply { stat: Nlm4Stats::Denied, holder: Some(holder) },
