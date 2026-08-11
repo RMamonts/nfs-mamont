cf913b9d2c46b34db11b857c5759ba0c1a9583c5
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-08-11 15:35:41 +0300

    Revert "feat(context): allow deferred backend attachment"
    This reverts commit f47a9938b0b248c8e56237931e9acdc423c192ba.

--- nfs-mamont/src/lib.rs
+++ nfs-mamont/src/lib.rs
@@ -25,7 +25,7 @@ use crate::{mount::Mount, task::connection};
 
 use crate::nlm::Nlm;
 pub use allocator::{Allocator, Buffer, Impl, Slice, UnownedBuffer};
-pub use context::{BackendHandle, ServerContext};
+pub use context::ServerContext;
 
 /// Initializes tracing logs.
 ///
