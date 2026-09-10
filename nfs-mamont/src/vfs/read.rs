c336f6d7484da8472947db040ba98d2c852cc7c8
Vadim Yarovoy <vadik.yarovoy@gmail.com>
2026-09-10 11:12:01 +0300

    docs(vfs): document READ reply count vs buffer size contract

--- nfs-mamont/src/vfs/read.rs
+++ nfs-mamont/src/vfs/read.rs
@@ -53,5 +53,9 @@ pub trait Read<B: Buffer> {
     ///
     /// The `data` buffer is allocated by NFS-Mamont allocator and must be
     /// filled by implementation. This keeps allocation policy under server control.
+    ///
+    /// The implementation must guarantee that [`SuccessHeader::count`] does not
+    /// exceed the size of the `data` buffer. Otherwise, a serialization error
+    /// will occur and the server will not be able to send a reply.
     async fn read(&self, args: Args, data: B) -> Result<Success<B>, Fail>;
 }
