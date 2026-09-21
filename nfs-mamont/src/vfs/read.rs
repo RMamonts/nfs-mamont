36304c62c960a12f8da9029c97472dcb47b795c4
svmk17 <svmk17@mail.ru>
2026-09-21 11:02:04 +0300

    fix: typo in read

--- nfs-mamont/src/vfs/read.rs
+++ nfs-mamont/src/vfs/read.rs
@@ -54,7 +54,7 @@ pub trait Read<B: Buffer> {
     ///
     /// The `data` buffer is allocated by NFS-Mamont allocator and must be
     /// filled by implementation. This keeps allocation policy under server control.
-    /// ///
+    ///
     /// The implementation must guarantee that [`SuccessHeader::count`] does not
     /// exceed the size of the `data` buffer. Otherwise, a serialization error
     /// will occur and the server will not be able to send a reply.
